c523b7ee4390308a72faf904e74185767e4460f9
CryptoHunter299 <96936412+CryptoHunter299@users.noreply.github.com>
2023-01-28 18:09:46 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,26 +1,18 @@
+from os import getenv
+from os.path import exists
 from dotenv import load_dotenv
-from os import path, getenv, mkdir
+
+load_dotenv("local.env" if exists("local.env") else None)
 
 
-if path.exists("local.env"):
-    load_dotenv("local.env")
-else:
-    load_dotenv()
-
-if not path.exists("search"):
-    mkdir("search")
-
-
-class Configs:
-    API_ID = '7166549'
-    API_HASH = '9a3151e9f5815a26b9fd7f447a1040a6'
-    BOT_TOKEN = '5951640980:AAFJz7Y8A2KZnpnbvdao4lEIGvwyWSVSuNg'
-    OWNER_ID = 1179359966
-    SESSION = 'BQB_tmX39Zy47B9eyH0hvG1JuDDj0_vkfNThT_XWRQ1DK5dWbVj0KYTfj1j2Ep8nJQmFKN_sw93hXffh_nn9yNb8ZnxG0alAx28Vg9pinyZb6t7yi5MVm0ULbU3swqNTO5XJkckanocoFBRW_TN-DYwe0OOAqwYjjAjwhYXDecI5CPHxG14-Xpq7icEj8k8_qNIrkAI70Pott15IiS4WKOeaZIJGAPv8PNrMlg1F7FKDjIXtkYQuSbEUocVnhHtrw5vAOnZyQIkzZ9Cn5HGlhM2a-I2FyuztSxpPFzp2Lau74OadRPcbK8O1BN48WvjHjghlb5QCpb_G9NZpObNEgy6ERskxzQA'
-    CHANNEL_LINK = 'https://t.me/C_DanaInstantPay'
-    GROUP_LINK = 'https://t.me/G_DanaInstantPay'
-    UPSTREAM_REPO = 'https://github.com/CryptoHunter299/CodingMusicBot'
-    AUTO_LEAVE = '1800'
+class Config:
+    api_id = int(getenv("API_ID", "0"))
+    api_hash = getenv("API_HASH")
+    bot_token = getenv("BOT_TOKEN")
+    session = getenv("SESSION", "session")
+    owner_id = int(getenv("OWNER_ID", "0"))
+    channel_link = getenv("CHANNEL_LINK", default="https://t.me/SolidProjectsReborn")
+    group_link = getenv("GROUP_LINK", default="https://t.me/SolidProjectsRebornChat")
 
 
-config = Configs()
+config = Config()
